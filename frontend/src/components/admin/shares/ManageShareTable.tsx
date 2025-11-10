c81de3cab514554c6bfb08624cbd1f9817ce8f4f
Nos4893 <80973975+Nos4893@users.noreply.github.com>
2025-11-10 20:47:49 +0100

    Refactor ManageShareTable to adjust column widths for improved layout

--- frontend/src/components/admin/shares/ManageShareTable.tsx
+++ frontend/src/components/admin/shares/ManageShareTable.tsx
@@ -41,12 +41,12 @@ const ManageShareTable = ({
       <Table verticalSpacing="sm">
         <thead>
           <tr>
-            <th style={{width: 220}}>
+            <th style={{width: 240}}>
               <FormattedMessage id="account.shares.table.name"/>
             </th>
 
             <MediaQuery smallerThan="xs" styles={{display: "none"}}>
-              <th style={{width: 220}}>
+              <th style={{width: 180}}>
                 <MediaQuery smallerThan="md" styles={{ display: 'none' }}>
                   <span><FormattedMessage id="account.shares.table.description" /></span>
                 </MediaQuery>
@@ -56,7 +56,7 @@ const ManageShareTable = ({
               </th>
             </MediaQuery>
 
-            <th style={{width: 220}}>
+            <th style={{width: 240}}>
               <MediaQuery smallerThan="md" styles={{ display: 'none' }}>
                 <span><FormattedMessage id="admin.shares.table.username" /></span>
               </MediaQuery>
